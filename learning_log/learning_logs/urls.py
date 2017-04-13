1766d14a7195127624a1ed275566d89734391115
JasonOnes <jasonr.jones14@gmail.com>
2017-04-12 17:21:05 -0700

    adding entries

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -13,4 +13,11 @@ urlpatterns = [
     url(r'^topics/$', views.topics, name='topics'),
     # Show a specific topic
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Add a new topic page
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # New entry page
+    url(r'^new_entry/(?P<topic_id>\d+)$', views.new_entry, name='new_entry'),
+    # Edit entry page
+    url(r'^edit_entry/(?P<entry_id>\d+)$', views.edit_entry,
+        name='edit_entry'),
 ]
